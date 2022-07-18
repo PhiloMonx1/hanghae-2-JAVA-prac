161ab6b6582ab0fe17a1f7c4323890f44a42e5fe
2m <philomonx1@gmail.com>
2022-07-18 16:43:23 +0900

    first commit

---
+++ src/Transport.java
@@ -0,0 +1,19 @@
+//### 상위 클래스 = 대중교통
+
+public class Transport {
+	int number;
+	int fuel = 100;
+	int speed = 0;
+	int changeSpeed;
+	int maxPassenger;
+	boolean status;
+}
+
+public void engineStart(){
+}
+public void changeSpeed(){
+}
+public void changeStatus(){
+}
+public void inPassenger(){
+}
